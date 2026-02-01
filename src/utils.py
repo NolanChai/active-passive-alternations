8cb672257a9407bc2dac83a01dcc56232371b59e
TQ <tianqiz12704@gmail.com>
2026-02-01 15:15:23 -0800

    feat: add files for word and sentence units + testing notebook

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+import conllu
+from pyinflect import getAllInflections, getInflection
+from .units.word import Word
+# from units.sentence import Sentence
+
+
+def switch_pronoun(const):
+    """ Switch relevant pronouns from subject to object.
+
+    Args:
+        const (list): list of Word objects to process
+    """
+    pron_key = {
+        "i": "me",
+        "he": "him",
+        "she": "her",
+        "we": "us",
+        "they": "them",
+        "me": "I",
+        "him": "he",
+        "her": "she",
+        "us": "we",
+        "them": "they"
+    }
+    for w in const:
+        # print(w['form'], w['deprel'])
+        if w['deprel'] in ['obl:agent', 'nsubj:pass'] and w['form'].lower() in pron_key:
+            w['form'] = pron_key[w['form'].lower()]
+
