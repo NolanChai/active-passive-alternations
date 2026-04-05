3790f02967e4fb3ccba702b3169deabfea60a86f
TQ <tianqiz12704@gmail.com>
2026-04-05 16:51:45 -0500

    chore: change experiment script to only run sentence-word experiment for now

--- run_experiments.sh
+++ run_experiments.sh
@@ -8,11 +8,11 @@ echo $MODEL_NAME
 echo $INPUT_DIR
 echo $OUTPUT_DIR
 
-.venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='word' --uid_level='document' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_word_document_uid.csv'
-.venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='token' --uid_level='document' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_token_document_uid.csv'
+# .venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='word' --uid_level='document' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_word_document_uid.csv'
+# .venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='token' --uid_level='document' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_token_document_uid.csv'
 .venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='word' --uid_level='sentence' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_word_sentence_uid.csv'
-.venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='token' --uid_level='sentence' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_token_sentence_uid.csv'
-.venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='word' --uid_level='(-10,+10)' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_word_(-10,+10)_uid.csv'
-.venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='token' --uid_level='(-10,+10)' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_token_(-10,+10)_uid.csv'
+# .venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='token' --uid_level='sentence' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_token_sentence_uid.csv'
+# .venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='word' --uid_level='(-10,+10)' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_word_(-10,+10)_uid.csv'
+# .venv/bin/python run_uid_pipeline.py $INPUT_DIR $MODEL_NAME --uid_unit='token' --uid_level='(-10,+10)' --generate_counterfactual --output_dir $OUTPUT_DIR --output_name 'cf_token_(-10,+10)_uid.csv'
 
 echo 'Experiment complete.'
