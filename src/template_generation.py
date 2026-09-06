5e0450ee360d016d0735c18186c284b276a0b46a
TQZhang04 <tianqiz12704@gmail.com>
2026-09-05 23:37:23 -0500

    refactor: rewrite data generation and processing code into functions

--- src/template_generation.py
+++ src/template_generation.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from itertools import permutations
+import numpy as np
+import torch
 
 def replace_map(string, mapping):
     for key, value in mapping.items():
@@ -42,3 +45,57 @@ def generate_variants(template,
             "text": text.replace("<PRE>", noun)
         })
     return pd.DataFrame(result)
+
+def generate_data(templates, names):
+    variants = []
+    for idx, row in templates.iterrows():
+        for perm in permutations(names):
+            for template in [row['passive'], row['active']]:
+                result = generate_variants(template, *perm, template==row['passive'])
+                result['pair_id'] = row['pair_id']
+                variants.append(result)
+        
+    variants = pd.concat(variants, ignore_index=True)
+    variants = variants.merge(templates.drop(columns=['passive', 'active']), on='pair_id', how='left')
+    return variants
+
+def process_results(model, tokenizer, device, variants, save_results_to_file=True):
+    uid_results = []
+    unigram = UnigramLM(tokenizer)
+    unigram.fit(" ".join(variants['text']), uid_unit="word")
+    for i in tqdm(range(variants.shape[0])):
+        try:
+            variant = variants.iloc[i]
+            text = variant['text']
+            preamble, sentence = text.split(". ")
+            preamble += "."
+            for context in [preamble, ""]:
+                tokens, surprisals = compute_surprisal(sentence, 
+                                context, 
+                                sentences=[context, sentence], 
+                                sent_idx=1, 
+                                tokenizer=tokenizer, 
+                                model=model,
+                                device=device)
+                surprisals, units = process_surprisals(tokenizer,
+                                                    tokens,
+                                                    surprisals,
+                                                    uid_unit="word")
+                uni_probs, _ = unigram(tokens)
+                result = get_uid_metrics(surprisals, uni_probs)
+                result['units'] = units
+                result['context'] = context
+                result['sentence'] = sentence
+                result.update(dict(variant))
+                uid_results.append(result)
+            if i % 100 == 0 & save_results_to_file:
+                # print(f"Processed {i} sentences")
+                pd.DataFrame(uid_results).to_csv("temp_results.csv", index=False)
+        except Exception as e:
+            print(f"Error processing sentence: {text}")
+            print(f"Error details: {e}")
+            continue
+    uid_results = pd.DataFrame(uid_results)
+    if save_results_to_file:
+        uid_results.to_csv("uid_results.csv", index=False)
+    return uid_results
